b9d497ea63c100d4fd7dce412417ebfee51b3a57
kshitij <kshitijdarwhekar@gmail.com>
2023-06-14 14:37:25 +0530

    App Skeleton

---
+++ flight-booking-website/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="App">
+        <h1>Flight Booking Website</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
